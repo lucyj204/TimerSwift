fbed1e59ed42478baaffb8d4a7de264490b14074
Lucy Joyce <lucyj20490@gmail.com>
2022-01-27 15:25:27 +0000

    Create UI for timer and link outlets and actions

--- TimerSwift/ViewController.swift
+++ TimerSwift/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var timerPicker: UIDatePicker!
+    private var timer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func timerValueChanged(_ sender: UIDatePicker) {
+        
+        
+    }
+    
+    
+    @IBAction func startPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cancelPressed(_ sender: UIButton) {
+        timer.invalidate()
+        progressBar.progress = 0.0
+        
+    }
 }
 
