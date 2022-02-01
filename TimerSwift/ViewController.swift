a2808cdb3d88d86d07ed0a15710e62abdf2ddbde
Lucy Joyce <lucyj20490@gmail.com>
2022-02-01 14:57:51 +0000

    Calculate timer duration from user input and display seconds remaining in timeLabel when start pressed

--- TimerSwift/ViewController.swift
+++ TimerSwift/ViewController.swift
@@ -13,15 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var timerPicker: UIDatePicker!
+    @IBOutlet weak var hourTextField: UITextField!
+    @IBOutlet weak var minuteTextField: UITextField!
+    @IBOutlet weak var secondsTextField: UITextField!
+    
+    
+    private var completionTime: Date?
     private var timer = Timer()
-    var duration = 0.0
-    var secondsPassed = 0.0
+    //    private let requestedCalendarComponent : Set<Calendar.Component> = [
+    //        Calendar.Component.hour,
+    //        Calendar.Component.minute,
+    //        Calendar.Component.second
+    //    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
     }
-    
     
     @IBAction func timerValueChanged(_ sender: UIDatePicker) {
         
@@ -29,27 +41,56 @@ class ViewController: UIViewController {
     }
     
     
+    
     @IBAction func startPressed(_ sender: UIButton) {
         
-        timer.invalidate()
-        progressBar.progress = 0.0
-        secondsPassed = 0
+        let hours = Int(hourTextField.text!) ?? 0
+        let hoursToSeconds = hours * 3600
+        let minutes = Int(minuteTextField.text!) ?? 0
+        let minutesToSeconds = minutes * 60
+        let seconds = Int(secondsTextField.text!) ?? 0
+        let duration = hoursToSeconds + minutesToSeconds + seconds
+        let now = Date()
+        self.completionTime = Calendar.current.date(byAdding: .second, value: duration, to: now)
         
-        duration = timerPicker.countDownDuration
-        let formatter = DateComponentsFormatter()
-        let formattedDuration = formatter.string(from: duration)
-        print(duration)
-        print(formattedDuration!)
+        let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
+        timer.fire()
         
-        let completionTime = Calendar.current.date(byAdding: .second, value: Int(duration), to: Date())
-        print(completionTime!)
-        
-    
-        
-        func cancelPressed(_ sender: UIButton) {
-            timer.invalidate()
-            
-        }
     }
+    
+    
+    @objc func updateCountdown() {
+        
+        let now = Date()
+        
+        let timeRemaining = calculateTimeDifference(startTime: now, completionTime: completionTime!)
+        
+        if timeRemaining > 0 {
+            timeLabel.text = "\(timeRemaining)"
+        } else {
+            timeLabel.text = "Timer done"
+            timer.invalidate()
+        }
+        
+    }
+    
+    
+    
+    func cancelPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+    }
+    
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
+func calculateTimeDifference(startTime: Date, completionTime: Date) -> TimeInterval {
+    
+    return completionTime.timeIntervalSinceNow - startTime.timeIntervalSinceNow
     
 }
