2538d8d24c1089ebbf5b720dd8bf5e9daba9dae2
Lucy Joyce <lucyj20490@gmail.com>
2022-02-01 18:47:11 +0000

    Fix error when cancelPressed, format timeLabel to show time remaining when timer is running and play sound when timer is finished

--- TimerSwift/ViewController.swift
+++ TimerSwift/ViewController.swift
@@ -6,40 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var timeLabel: UILabel!
-    @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var timerPicker: UIDatePicker!
     @IBOutlet weak var hourTextField: UITextField!
     @IBOutlet weak var minuteTextField: UITextField!
     @IBOutlet weak var secondsTextField: UITextField!
     
-    
     private var completionTime: Date?
     private var timer = Timer()
-    //    private let requestedCalendarComponent : Set<Calendar.Component> = [
-    //        Calendar.Component.hour,
-    //        Calendar.Component.minute,
-    //        Calendar.Component.second
-    //    ]
+    private var player: AVAudioPlayer!
+    
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        view.isUserInteractionEnabled = true
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        
         view.addGestureRecognizer(tap)
     }
-    
-    @IBAction func timerValueChanged(_ sender: UIDatePicker) {
-        
-        
-    }
-    
     
     
     @IBAction func startPressed(_ sender: UIButton) {
@@ -53,38 +41,57 @@ class ViewController: UIViewController {
         let now = Date()
         self.completionTime = Calendar.current.date(byAdding: .second, value: duration, to: now)
         
-        let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: true)
         timer.fire()
         
     }
     
-    
     @objc func updateCountdown() {
         
         let now = Date()
-        
         let timeRemaining = calculateTimeDifference(startTime: now, completionTime: completionTime!)
+        let displayHours = Int(timeRemaining) / 3600
+        let displayMinutes = Int(timeRemaining) / 60 % 60
+        let displaySeconds = Int(timeRemaining) % 60
         
         if timeRemaining > 0 {
-            timeLabel.text = "\(timeRemaining)"
+            
+            timeLabel.text = String(format: "%02i:%02i:%02i", displayHours, displayMinutes, displaySeconds)
+            hourTextField.text = ""
+            minuteTextField.text = ""
+            secondsTextField.text = ""
+            
         } else {
-            timeLabel.text = "Timer done"
+            
+            timeLabel.text = "00:00:00"
+            playSound()
             timer.invalidate()
+            
         }
-        
     }
     
     
-    
-    func cancelPressed(_ sender: UIButton) {
+    @IBAction func cancelPressed(_ sender: UIButton) {
         
-        timer.invalidate()
+        completionTime = Date()
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateCountdown), userInfo: nil, repeats: false)
         
     }
     
     
     @objc func dismissKeyboard() {
+        
         view.endEditing(true)
+        print("tap gesture recognised")
+        
+    }
+    
+    func playSound() {
+        
+        let url = Bundle.main.url(forResource: "kitty", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
     }
 }
 
@@ -94,3 +101,4 @@ func calculateTimeDifference(startTime: Date, completionTime: Date) -> TimeInter
     return completionTime.timeIntervalSinceNow - startTime.timeIntervalSinceNow
     
 }
+
