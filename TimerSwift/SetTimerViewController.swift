7d22d7fdf2384f60f5f0f9d526291fde35e38791
Lucy Joyce <lucyj20490@gmail.com>
2022-03-04 12:42:48 +0000

    Add fixed labels for hr, min, sec on setTimer view

--- TimerSwift/SetTimerViewController.swift
+++ TimerSwift/SetTimerViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class SetTimerViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var timerCreationView: UIView!
-    @IBOutlet weak var selectTimerLengthLabel: UILabel!
     @IBOutlet weak var timerPickerView: UIPickerView!
     @IBOutlet weak var setTimerButton: UIButton!
     @IBOutlet weak var cancelTimerButton: UIButton!
@@ -37,26 +36,14 @@ class SetTimerViewController: UIViewController, UITextFieldDelegate {
         return false
     }
     
-    func updateSelectedTimeLabel() {
-        let hourTime = timerPickerView.selectedRow(inComponent: 0)
-        let minuteTime = timerPickerView.selectedRow(inComponent: 2)
-        let secondTime = timerPickerView.selectedRow(inComponent: 4)
-        
-        if secondTime == 0 && minuteTime == 0 && hourTime == 0 {
-            selectTimerLengthLabel.text = "Select timer length:"
-        } else {
-            selectTimerLengthLabel.text = String(format: "%02i:%02i:%02i", hourTime, minuteTime, secondTime)
-        }
-    }
-    
     @IBAction func setTimerPressed(_ sender: UIButton) {
         let alert = UIAlertController(title: "Alert", message: "Please select timer duration", preferredStyle: .alert)
         
         let hourTime = timerPickerView.selectedRow(inComponent: 0)
         let hoursToSecondsTime = hourTime * 3600
-        let minuteTime = timerPickerView.selectedRow(inComponent: 2)
+        let minuteTime = timerPickerView.selectedRow(inComponent: 1)
         let minutesToSecondsTime = minuteTime * 60
-        let secondTime = timerPickerView.selectedRow(inComponent: 4)
+        let secondTime = timerPickerView.selectedRow(inComponent: 2)
         let duration = hoursToSecondsTime + minutesToSecondsTime + secondTime
         let name = timerNameTextField.text!
         
@@ -85,8 +72,9 @@ class SetTimerViewController: UIViewController, UITextFieldDelegate {
 }
 
 extension SetTimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        updateSelectedTimeLabel()
+//        updateSelectedTimeLabel()
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -95,30 +83,51 @@ extension SetTimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
-        case 1, 3, 5: return 1
         case 0: return 60
+        case 1: return 60
         case 2: return 60
-        case 4: return 60
         default: return 0
         }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let font = UIFont.systemFont(ofSize: 20.0)
+        let fontSize: CGFloat = font.pointSize
+        let componentWidth: CGFloat = self.view.frame.width / CGFloat(timerPickerView.numberOfComponents)
+        let y = (timerPickerView.frame.size.height / 2) - (fontSize / 2)
+        
+        let hourLabel = UILabel(frame: CGRect(x: componentWidth * 1.6, y: y, width: componentWidth * 0.5, height: fontSize))
+        hourLabel.font = font
+        hourLabel.textAlignment = .left
+        hourLabel.text = "Hr"
+        hourLabel.textColor = UIColor.white
+        timerPickerView.addSubview(hourLabel)
+        
+        let minuteLabel = UILabel(frame: CGRect(x: componentWidth * 2.65, y: y, width: componentWidth * 0.5, height: fontSize))
+        minuteLabel.font = font
+        minuteLabel.textAlignment = .left
+        minuteLabel.text = "Min"
+        minuteLabel.textColor = UIColor.white
+        timerPickerView.addSubview(minuteLabel)
+        
+        let secondLabel = UILabel(frame: CGRect(x: componentWidth * 3.7, y: y, width: componentWidth * 0.6, height: fontSize))
+        secondLabel.font = font
+        secondLabel.textAlignment = .left
+        secondLabel.text = "Sec"
+        secondLabel.textColor = UIColor.white
+        timerPickerView.addSubview(secondLabel)
+        
         switch component {
         case 0: return "\(row)"
-        case 1: return "h"
+        case 1: return "\(row)"
         case 2: return "\(row)"
-        case 3: return "m"
-        case 4: return "\(row)"
-        case 5: return "s"
         default: return ""
         }
     }
     
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         switch component {
-        case 1, 3, 5: return self.view.frame.width * 0.13
-        case 0, 2, 4: return self.view.frame.width * 0.13
+        case 0, 1, 2: return self.view.frame.width * 0.17
         default: return 0
         }
     }
